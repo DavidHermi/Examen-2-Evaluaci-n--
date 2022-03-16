cb01d2d0b64ca2f5c973a2a92543d70a10c1a33a
DavidHermi <dhermidaperez@danielcastelao.org>
2022-03-16 14:21:59 +0100

    Codigo inicial

---
+++ src/main/java/main.java
@@ -0,0 +1,31 @@
+import discord4j.core.DiscordClient;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.channel.MessageChannel;
+
+public final class main {
+
+
+    public static void main(final String[] args) {
+
+            final String token = "d1447ce73859ba6b39bb1b7a213ce2e99bd330c697c8868d9f20a8c0a3714e1b";
+            final DiscordClient client = DiscordClient.create(token);
+            final GatewayDiscordClient gateway = client.login().block();
+
+            gateway.on(MessageCreateEvent.class).subscribe(event -> {
+                final Message message = event.getMessage();
+                if ("!ping".equals(message.getContent())) {
+                    final MessageChannel channel = message.getChannel().block();
+                    channel.createMessage("Pong!").block();
+                }
+            });
+
+            gateway.onDisconnect().block();
+
+    }
+
+
+
+
+}
